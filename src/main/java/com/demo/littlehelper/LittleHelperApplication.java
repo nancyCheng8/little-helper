64ffbc0ecbc780f4033947a1df84273225a7782d
nancy.cheng <nancy820804@gmail.com>
2024-06-23 11:35:49 +0800

    init demo

---
+++ src/main/java/com/demo/littlehelper/LittleHelperApplication.java
@@ -0,0 +1,13 @@
+package com.demo.littlehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleHelperApplication.class, args);
+	}
+
+}
